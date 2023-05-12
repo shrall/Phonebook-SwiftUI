3ff05df45e3deb6a859ba45c3ea05107498d277d
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-05-12 11:42:31 +0700

    Initial Commit

---
+++ Phonebook-SwiftUI/Phonebook_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phonebook_SwiftUIApp.swift
+//  Phonebook-SwiftUI
+//
+//  Created by Marshall Kurniawan on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Phonebook_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
